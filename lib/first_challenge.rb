b8a9488e5e6026ed16fe7111140a2f4f105a6ed7
Faraz Hanif <hfaraz745@gmail.com>
2018-04-19 23:24:06 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -13,12 +13,8 @@ def first_challenge
     }
   }
 
-  contacts["jon snow"][:favorite_icecream_flavors][0]
-  contacts["jon snow"][:favorite_icecream_flavors][1]
-  contacts["jon snow"][:favorite_icecream_flavors][2]
-
+  
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
